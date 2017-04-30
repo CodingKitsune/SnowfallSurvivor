b376e41950be7477dca5cf8950322eee2614c543
Willian Azevedo <Willian Azevedo@10.24.8.177>
2017-04-30 07:54:18 -0300

    map near the ocean

---
+++ main/levels/level_near_the_ocean/level_near_the_ocean.go
@@ -0,0 +1,15 @@
+components {
+  id: "level_near_the_ocean"
+  component: "/main/levels/level_near_the_ocean/level_near_the_ocean.collectionproxy"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
