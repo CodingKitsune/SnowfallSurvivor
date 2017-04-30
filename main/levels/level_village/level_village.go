eebd87d9a90a3d33957b4c66479bdb04207bbb4d
kit <codingkitsune@gmail.com>
2017-04-30 08:03:37 -0300

    v0.4

--- main/levels/level_village/level_village.go
+++ main/levels/level_village/level_village.go
@@ -13,3 +13,30 @@ components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "collision_shape: \"/main/levels/level_village/level_village.tilemap\"\n"
+  "type: COLLISION_OBJECT_TYPE_DYNAMIC\n"
+  "mass: 1.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"world\"\n"
+  "mask: \"player\"\n"
+  "mask: \"enemy\"\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: true\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
